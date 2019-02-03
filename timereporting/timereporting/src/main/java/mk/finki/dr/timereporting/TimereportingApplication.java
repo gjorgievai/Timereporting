53545c6b79a6875d47dec4bfeb49f82397215ff5
kajtazovai <kajtazova@gmail.com>
2019-02-03 22:48:15 +0100

    Initial commit

---
+++ timereporting/timereporting/src/main/java/mk/finki/dr/timereporting/TimereportingApplication.java
@@ -0,0 +1,14 @@
+package mk.finki.dr.timereporting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimereportingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimereportingApplication.class, args);
+	}
+
+}
+
